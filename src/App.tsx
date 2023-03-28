5649a74177e3147451dcabee187b4c15885a61d9
Manuel Di Campli <manuel.dicampli@rocketmail.com>
2023-03-28 17:12:54 +0200

    new branch develop test

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
 	};
 
 	return (
-		<div className="container d-flex flex-column py-5 gap-4">
+		<div className="container d-flex flex-column py-5 gap-4 p-5">
 			<h1 className="text-center">CRUD React + Firebase</h1>
 			<div className="row gap-5">
 				<div className="col pe-md-5">
